bf5779c3c75da1df38c2d2dd01e89bf36f19b6df
Arkadiusz Skalski <adiusz.arek@gmail.com>
2019-01-08 19:57:32 +0100

    LAB 6 - multithread

--- main.cpp
+++ main.cpp
@@ -10,11 +10,12 @@ const int MAX = 100;
 
 HANDLE create_file();
 DWORD write_file(HANDLE, string);
-HANDLE new_process();
+HANDLE new_process(int);
 void random_numbers_generator();
 void random_numbers(int*, int, int, int);
 int single_random_number(int, int);
 DWORD print_result(HANDLE, int*, int);
+HANDLE create_result(int, int, int);
 
 int main(int argc, char* argv[])
 {
@@ -22,30 +23,67 @@ int main(int argc, char* argv[])
 
 		cout << "Program needs arguments" << endl;
 	} else {
-
+		
+		LARGE_INTEGER liDueTime;
         int quantity = atoi(argv[1]);
+		int seconds = atoi(argv[2]);
+		int counter = atoi(argv[3]);
         int* randomIntArray = new int[quantity];
         
-        random_numbers_generator();
-		random_numbers(randomIntArray, quantity, MIN, MAX);
+        liDueTime.QuadPart = -(seconds * 10000000);
+		HANDLE Timer = NULL;
+		HANDLE result = create_result(quantity, counter, seconds);
+		CloseHandle(result);
+		
+		int i = 0, frequency = 1;
         
-		HANDLE out = create_file();
-		
-		if (out == INVALID_HANDLE_VALUE) {
-        	cout << "CreateFile error " << GetLastError() << "." << endl;
-            getchar();
-            return 1;
-		}
-		
-		HANDLE second_process = new_process();
-		
-		print_result(out, randomIntArray, quantity);
-		
-		CloseHandle(out);
-		
-		WaitForSingleObject(second_process, INFINITE);
-		
-		CloseHandle(second_process);
+        do {
+	        random_numbers_generator();
+			random_numbers(randomIntArray, quantity, MIN, MAX);
+	        
+			HANDLE out = create_file();
+			
+			if (out == INVALID_HANDLE_VALUE) {
+	        	cout << "CreateFile error " << GetLastError() << "." << endl;
+	            getchar();
+	            return 1;
+			}
+			
+			Timer = CreateWaitableTimer(NULL, TRUE, NULL);
+			
+			if(Timer == NULL) {
+				cout << "CreateWaitableTimer failed " << GetLastError() << endl;
+        		return 1;
+			}
+ 			
+    		cout << "\nWaiting for " << seconds << " seconds." << endl;	
+    		
+			if(!SetWaitableTimer(Timer, &liDueTime, 0, NULL, NULL, 0)) {
+				cout << "SetWaitableTimer failed " << GetLastError() << endl;
+				return 1;
+    		}
+    	
+    		if(WaitForSingleObject(Timer, INFINITE) != WAIT_OBJECT_0) {
+				cout << "WaitForSingleObject failed " << GetLastError() << endl;
+			} else {
+				cout << "\nTimer was signaled.\n" << endl;
+			}
+			
+			HANDLE second_process = new_process(frequency);
+			
+			print_result(out, randomIntArray, quantity);
+			
+			CloseHandle(out);
+			
+			WaitForSingleObject(second_process, INFINITE);
+			
+			CloseHandle(second_process);
+	
+			i++;
+			frequency++;
+		} while(!(WaitForSingleObject(Timer, INFINITE) != WAIT_OBJECT_0) && (i != counter));
+			
+		free(randomIntArray);
 	}
 
 	return 0;
@@ -100,7 +138,11 @@ int single_random_number(int MIN, int MAX) {
 	return randomInt;
 }
 
-HANDLE new_process() {
+HANDLE new_process(int frequency) {
+	string one = "program.exe ";
+	string two = to_string(frequency);
+	string finally = one + two;
+	const char * data = finally.c_str();
 	
 	STARTUPINFO startupinfo;
     PROCESS_INFORMATION process_information;
@@ -112,7 +154,7 @@ HANDLE new_process() {
 	/**
 	*	Creating a separate process
 	**/
-    if(!CreateProcess("program.exe", NULL, NULL, NULL, FALSE,0, NULL, NULL, &startupinfo, &process_information)) {
+    if(!CreateProcess("program.exe", (char*)data, NULL, NULL, FALSE, 0, NULL, NULL, &startupinfo, &process_information)) {
     	
         printf("CreateProcess failed (%d).\n", GetLastError());
 	}
@@ -135,5 +177,30 @@ DWORD print_result(HANDLE hFile, int* array, int quantity) {
 	DWORD bytesWritten = write_file(hFile, toWrite);
 	
 	return bytesWritten;
+}
+
+HANDLE create_result(int quantity, int replies, int pause) {
+	
+	HANDLE file = CreateFile(TEXT("result.txt"), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if(file == INVALID_HANDLE_VALUE) {
+        printf("CreateFile error %d.\n", GetLastError());
+        getchar();
+	} 
+		
+	_SYSTEM_INFO LPSYSTEM_INFO;
+	GetSystemInfo(&LPSYSTEM_INFO);
+	
+	string tabulator = "\t ";
+	string enter = "\r\n";
+	string toWrite = "Total quantity: "+to_string(quantity)+" | Version: Multithread | Machine: "+to_string(LPSYSTEM_INFO.dwProcessorType)
+		+enter+"Replies: "+to_string(replies)+tabulator+" | Pause: "+to_string(pause)+" sec"+enter+enter+"Nr."+tabulator+"Avg"+tabulator+tabulator+
+		"Min"+tabulator+"Max"+enter;
+		
+	DWORD bytesWritten = 0;
+	bool isWrittenSuccees = WriteFile(file, &toWrite[0], sizeof(char)*toWrite.length(), &bytesWritten, NULL);
+										
+	if(!isWrittenSuccees)	cout << "There was not posibility to write to file." << endl;
+	
+	return file;
 }
 
--- program.cpp
+++ program.cpp
@@ -4,15 +4,30 @@
 #include <vector>
 #include <windows.h>
 #include <algorithm>
+#define MAX_THREADS 3
 
 using namespace std;
 
+DWORD INVALID_SET_FILE = 0xFFFFFFFF;
+
+typedef struct MyData {
+	vector<int> myNumbers;
+	float average = -1;
+	int max = -1;
+	int min = 100;
+	
+} MYDATA, *PMYDATA;
+
 HANDLE create_file();
 DWORD read_file(HANDLE, char[], int);
-DWORD WINAPI AllOperation(LPVOID);
+void update_result(HANDLE, MyData*, int);
+DWORD WINAPI Min(LPVOID);
+DWORD WINAPI Max(LPVOID);
+DWORD WINAPI Average(LPVOID);
 
 int main(int argc, char* argv[])
 {
+	int frequency = atoi(argv[1]);
 	HANDLE out;
 	
 	do {
@@ -21,6 +36,19 @@ int main(int argc, char* argv[])
 	} while(out == INVALID_HANDLE_VALUE);
 	
 	cout << "File has been loaded." << endl;
+	
+	HANDLE resultFile = CreateFile(TEXT("result.txt"), GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if(resultFile == INVALID_HANDLE_VALUE) {
+        printf("CreateFile error %d.\n", GetLastError());
+        getchar();
+	}
+	
+	DWORD Pointer = SetFilePointer(resultFile, NULL, NULL, FILE_END);
+	if(Pointer == INVALID_SET_FILE) {
+		cout << "SetFilePointer error " << GetLastError() << endl;
+    	getchar();
+		return 1;
+	}
 	
 	DWORD byteSize = GetFileSize(out, NULL);
 	
@@ -46,10 +74,33 @@ int main(int argc, char* argv[])
 		cout << myNumbers.at(i) << "\t";
 	}
 
-	DWORD ID;
-	HANDLE thread = CreateThread(NULL, 0, AllOperation, &myNumbers, 0, &ID);
-	WaitForSingleObject(thread, INFINITE);
-	CloseHandle(thread);
+	HANDLE ThreadArray[MAX_THREADS]; 
+	DWORD ID[MAX_THREADS];
+	PMYDATA StructOfDataForThread;
+	
+	HANDLE Heap = GetProcessHeap();
+    if(Heap == NULL) {
+        cout << "Heaping Heap error: " << GetLastError() << endl;
+        ExitProcess(2);
+    }
+    
+    StructOfDataForThread = (PMYDATA) HeapAlloc(Heap, HEAP_ZERO_MEMORY, sizeof(MYDATA));
+    if (StructOfDataForThread == NULL)	ExitProcess(2);
+    StructOfDataForThread->myNumbers = myNumbers;
+
+	ThreadArray[0] = CreateThread(NULL, 0, Max, StructOfDataForThread, CREATE_SUSPENDED, &ID[0]);
+	ThreadArray[1] = CreateThread(NULL, 0, Min, StructOfDataForThread, CREATE_SUSPENDED, &ID[1]);
+	ThreadArray[2] = CreateThread(NULL, 0, Average, StructOfDataForThread, CREATE_SUSPENDED, &ID[2]);
+	
+	for (int i = 0; i < MAX_THREADS; i++) {
+		ResumeThread(ThreadArray[i]);
+		WaitForSingleObject(ThreadArray[i], INFINITE);
+		CloseHandle(ThreadArray[i]);
+	}
+	
+	update_result(resultFile, StructOfDataForThread, frequency);
+	if (!(HeapFree(Heap, 0, StructOfDataForThread)))	cout << "Failed to free allocation from Heap." << endl;
+	
 	cout << "\nThe thread has been closed successfully." << endl;
 
 	return 0;
@@ -78,11 +129,33 @@ DWORD read_file(HANDLE out, char data[], int size) {
 	return bytesRead;
 }
 
-DWORD WINAPI AllOperation(LPVOID value){
-	
-	vector<int> myNumbers = *((vector<int>*)value);
-	vector<int>::iterator it1 = max_element(myNumbers.begin(), myNumbers.end());
-	vector<int>::iterator it2 = min_element(myNumbers.begin(), myNumbers.end());
+DWORD WINAPI Max(LPVOID value) {
+
+	PMYDATA DataForThread = (PMYDATA)value;
+	vector<int> myNumbers = DataForThread->myNumbers;
+	vector<int>::iterator it = max_element(myNumbers.begin(), myNumbers.end());
+
+	cout << "\nMaximum value is: " << *it << endl;
+	DataForThread->max = int(*it);
+
+	return 0;
+}
+
+DWORD WINAPI Min(LPVOID value) {
+
+	PMYDATA DataForThread = (PMYDATA)value;
+	vector<int> myNumbers = DataForThread->myNumbers;
+	vector<int>::iterator it = min_element(myNumbers.begin(), myNumbers.end());
+
+	cout << "Minimum value is: " << *it << endl;
+	DataForThread->min = int(*it);
+	return 0;
+}
+
+DWORD WINAPI Average(LPVOID value) {
+
+	PMYDATA DataForThread = (PMYDATA)value;
+	vector<int> myNumbers = DataForThread->myNumbers;
 	
 	int size = myNumbers.size();
 	float sum = 0;
@@ -92,10 +165,18 @@ DWORD WINAPI AllOperation(LPVOID value){
 	
 	float result = sum/size;
 
-	cout << "\n\nThe average is: " << result << endl;
-	cout << "Maximum value is: " << *it1 << endl;
-	cout << "Minimum value is: " << *it2 << endl;
-	
+	cout << "The average is: " << result << endl;
+	DataForThread->average = result;
 	return 0;
 }
 
+void update_result(HANDLE file, MyData* data, int frequency) {
+	string tabulator = "\t ";
+	string enter = "\r\n";
+	string toWrite = to_string(frequency)+tabulator+to_string(data->average)+tabulator+to_string(data->min)+tabulator+to_string(data->max)+enter;
+	
+	DWORD bytesWritten = 0;
+	bool isWrittenSuccees = WriteFile(file, &toWrite[0], sizeof(char)*toWrite.length(), &bytesWritten, NULL);
+										
+	if(!isWrittenSuccees)	cout << "There was not posibility to write to file." << endl;
+}
